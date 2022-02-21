f9e588bcdce0a7cac758d59a9e5390c0a342bed8
Radenz <radenrifqirahman@gmail.com>
2022-02-22 00:16:18 +0700

    chore: Add documentation generator script

---
+++ build/docs.py
@@ -0,0 +1,21 @@
+import os
+from os.path import basename
+
+base = basename(os.getcwd())
+
+if (base == "build"):
+    os.chdir("..")
+
+print("\033[38;5;14mGenerating package documentation...\033[0m")
+cmd = [
+    "pdoc3",
+    "--html",
+    "--force",
+    "-o",
+    "docs",
+    "-c",
+    "show_source_code=False",
+    "myConvexHull"
+]
+
+os.system(" ".join(cmd))
