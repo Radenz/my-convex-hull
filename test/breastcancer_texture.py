ab33c3e6d3d1d9d6f89e6a6c737531ebf21b6ee6
Radenz <radenrifqirahman@gmail.com>
2022-02-27 16:15:41 +0700

    test: Use randomized color for every test

--- test/breastcancer_texture.py
+++ test/breastcancer_texture.py
@@ -2,20 +2,14 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn import datasets
-from myConvexHull import convex_hull
+from myConvexHull import convex_hull, random_color
 from myConvexHull.point_utils import X, Y
 
 data = datasets.load_breast_cancer()
-
 df = pd.DataFrame(data.data, columns=data.feature_names)
 df['Target'] = pd.DataFrame(data.target)
-
 plt.figure(figsize=(10, 6))
-
-colors = ['b', 'r', 'g']
-
 plt.title('Mean Radius vs Mean Texture')
-
 plt.xlabel(data.feature_names[0])
 plt.ylabel(data.feature_names[1])
 
@@ -23,11 +17,11 @@ for i in range(len(data.target_names)):
     bucket = df[df['Target'] == i]
     bucket = bucket.iloc[:, [0, 1]].values
     hull = convex_hull(bucket)
-
-    plt.scatter(bucket[:, 0], bucket[:, 1], label=data.target_names[i])
-
+    color = random_color()
+    plt.scatter(bucket[:, 0], bucket[:, 1],
+                label=data.target_names[i], color=color)
     hull = np.transpose(hull)
-    plt.plot(hull[X], hull[Y], colors[i])
+    plt.plot(hull[X], hull[Y], color=color)
 
 plt.legend()
 plt.show()
--- test/digits_1112.py
+++ test/digits_1112.py
@@ -6,14 +6,10 @@ from myConvexHull import convex_hull, random_color
 from myConvexHull.point_utils import X, Y
 
 data = datasets.load_digits()
-
 df = pd.DataFrame(data.data, columns=data.feature_names)
 df['Target'] = pd.DataFrame(data.target)
-
 plt.figure(figsize=(10, 6))
-
 plt.title('Pixel [1, 3] vs Pixel [1, 4]')
-
 plt.xlabel(data.feature_names[11])
 plt.ylabel(data.feature_names[12])
 
@@ -21,12 +17,9 @@ for i in range(len(data.target_names)):
     bucket = df[df['Target'] == i]
     bucket = bucket.iloc[:, [11, 12]].values
     hull = convex_hull(bucket)
-
     color = random_color()
-
-    plt.scatter(bucket[:, 0], bucket[:, 1], label=data.target_names[i],
-                color=color)
-
+    plt.scatter(bucket[:, 0], bucket[:, 1],
+                label=data.target_names[i], color=color)
     hull = np.transpose(hull)
     plt.plot(hull[X], hull[Y], color=color)
 
